0b36db7f732245530c55cd7919d1bbc5310b9648
giorgos-grekis <georgrekis@gmail.com>
2024-10-08 22:35:53 +0300

    create a menu

--- lesson_02/tailwind.config.js
+++ lesson_02/tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+        tailscreen: { raw: "(min-aspect-ratio: 13/20)" },
+      },
+    },
+    Keyframes: {
+      "open-menu": {
+        "0%": { transform: "scaleY(0)" },
+        "80%": { transform: "scaleY(1.2)" },
+        "100%": { transform: "scaleY(1)" },
+      },
+    },
+
+    animation: {
+      "open-menu": "open-menu 0.5s ease-in-out forwards",
+    },
   },
   plugins: [],
-}
-
+};
